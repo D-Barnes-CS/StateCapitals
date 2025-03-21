93120cfb4def99f6a21c0b14dda005e7f1958583
D-Barnes-CS <dylan27b@gmail.com>
2025-03-21 14:58:01 -0400

    state capitals updated - with Rekha's help

--- app/src/main/java/com/dbarnes/statecapitals/MainActivity.kt
+++ app/src/main/java/com/dbarnes/statecapitals/MainActivity.kt
@@ -8,12 +8,12 @@ import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    val viewModel: CapitalViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        val viewModel: CapitalViewModel by viewModels()
         displayRandomStateCapital()
 
         binding.nextButton.setOnClickListener {
