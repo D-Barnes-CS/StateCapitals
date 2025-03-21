aa73b66d8c307ab676caa41507fa1b1f8702d317
D-Barnes-CS <dylan27b@gmail.com>
2025-03-21 14:36:40 -0400

    state capitals updated

--- app/src/main/java/com/dbarnes/statecapitals/Capital.kt
+++ app/src/main/java/com/dbarnes/statecapitals/Capital.kt
@@ -1,21 +1,65 @@
 package com.dbarnes.statecapitals
+
+import androidx.lifecycle.ViewModel
+import kotlin.random.Random
+
 //class header, including primary constructor
-data class Capital(val state: String, val capitalCity: String) {
-    //properties
-    var state : String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
-    var capitalCity : String = ""
-        get() = field
-        set(value) {
-            field = value
-        }
-    //set the property values to the corresponding
-//parameters of the primary constructor
-    init {
-        this.state = state
-        this.capitalCity = capitalCity
+data class Capital(val state: String, val capitalCity: String)
+//https://developer.android.com/topic/libraries/architecture/viewmodel
+class CapitalViewModel : ViewModel() {
+    private val stateList = listOf(
+        Capital("Alabama", "Montgomery"),
+        Capital("Alaska", "Juneau"),
+        Capital("Arizona", "Phoenix"),
+        Capital("Arkansas", "Little Rock"),
+        Capital("California", "Sacramento"),
+        Capital("Colorado", "Denver"),
+        Capital("Connecticut", "Hartford"),
+        Capital("Delaware", "Dover"),
+        Capital("Hawaii", "Honolulu"),
+        Capital("Florida", "Tallahassee"),
+        Capital("Georgia", "Atlanta"),
+        Capital("Idaho", "Boise"),
+        Capital("Illinois", "Springfield"),
+        Capital("Indiana", "Indianapolis"),
+        Capital("Iowa", "Des Moines"),
+        Capital("Kansas", "Topeka"),
+        Capital("Kentucky", "Frankfort"),
+        Capital("Louisiana", "Baton Rouge"),
+        Capital("Maine", "Augusta"),
+        Capital("Maryland", "Annapolis"),
+        Capital("Massachusetts", "Boston"),
+        Capital("Michigan", "Lansing"),
+        Capital("Minnesota", "St. Paul"),
+        Capital("Mississippi", "Jackson"),
+        Capital("Missouri", "Jefferson City"),
+        Capital("Montana", "Helena"),
+        Capital("Nebraska", "Lincoln"),
+        Capital("Nevada", "Carson City"),
+        Capital("New Hampshire", "Concord"),
+        Capital("New Jersey", "Trenton"),
+        Capital("New Mexico", "Santa Fe"),
+        Capital("North Carolina", "Raleigh"),
+        Capital("North Dakota", "Bismarck"),
+        Capital("New York", "Albany"),
+        Capital("Ohio", "Columbus"),
+        Capital("Oklahoma", "Oklahoma City"),
+        Capital("Oregon", "Salem"),
+        Capital("Pennsylvania", "Harrisburg"),
+        Capital("Rhode Island", "Providence"),
+        Capital("South Carolina", "Columbia"),
+        Capital("South Dakota", "Pierre"),
+        Capital("Tennessee", "Nashville"),
+        Capital("Texas", "Austin"),
+        Capital("Utah", "Salt Lake City"),
+        Capital("Vermont", "Montpelier"),
+        Capital("Virginia", "Richmond"),
+        Capital("Washington", "Olympia"),
+        Capital("West Virginia", "Charleston"),
+        Capital("Wisconsin", "Madison"),
+        Capital("Wyoming", "Cheyenne")
+    )
+    fun getRandomStateCapital(): Capital {
+        return stateList[Random.nextInt(stateList.size)]
     }
 }
--- app/src/main/java/com/dbarnes/statecapitals/MainActivity.kt
+++ app/src/main/java/com/dbarnes/statecapitals/MainActivity.kt
@@ -3,12 +3,12 @@ package com.dbarnes.statecapitals
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dbarnes.statecapitals.databinding.ActivityMainBinding
-import android.util.Log
-import kotlin.random.Random
+import androidx.activity.viewModels
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: CapitalViewModel by viewModels()
+    val viewModel: CapitalViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun displayRandomStateCapital() {
         val randomCapital = viewModel.getRandomStateCapital()
-        var messageString = "${randomCapital.capitalCity} " +
+        val messageString = "${randomCapital.capitalCity} " +
                 "is the capital of ${randomCapital.state}"
         binding.capitalInfo.text = messageString
     }
